bb0474c237794766e88a3e0ba59b4f69d1be0ea6
shaoli-lu <shaoli_lu@yahoo.com>
2022-02-27 19:35:33 -0600

    initial

---
+++ script.js
@@ -0,0 +1,78 @@
+const video = document.querySelector("video")
+const btn = document.getElementById("btn")
+const textElem = document.querySelector("[data-text]")
+video.setAttribute('autoplay', '');
+video.setAttribute('muted', '');
+video.setAttribute('playsinline', '');
+async function setup() {
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+  video.srcObject = stream
+
+  video.addEventListener("playing", async () => {
+    const worker = Tesseract.createWorker()
+    await worker.load()
+    await worker.loadLanguage("eng")
+    await worker.initialize("eng")
+
+    const canvas = document.createElement("canvas")
+    canvas.width = video.width
+    canvas.height = video.height
+
+      // document.addEventListener("keypress", async e => {
+      // if (e.code == "Backspace") return
+      btn.addEventListener("click", async e => {
+    
+      canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height)
+      const {
+        data: { text },
+      } = await worker.recognize(canvas)
+
+      var msg = new SpeechSynthesisUtterance(text.replace(/\s/g, " "));
+     // msg.lang = 'zh';
+      speechSynthesis.speak(
+        msg
+      )
+
+      textElem.textContent = text
+    })
+
+     document.addEventListener("keypress", async e => {
+      if (e.code == "Backspace") return
+    
+        canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height)
+        const {
+          data: { text },
+        } = await worker.recognize(canvas)
+  
+        var msg = new SpeechSynthesisUtterance(text.replace(/\s/g, " "));
+       // msg.lang = 'zh';
+        speechSynthesis.speak(
+          msg
+        )
+  
+        textElem.textContent = text
+      })
+  
+    // btn.addEventListener("click", async e => {
+    //   return
+    //   canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height)
+    //   const {
+    //     data: { text },
+    //   } = await worker.recognize(canvas)
+
+    //   var msg = new SpeechSynthesisUtterance(text.replace(/\s/g, " "));
+    //   msg.lang = 'zh';
+    //   speechSynthesis.speak(
+    //     msg
+    //   )
+
+    //   textElem.textContent = text
+    // })
+
+  })
+}
+
+setup()
+// btn.addEventListener('click', function(){
+//   setup();
+// });
